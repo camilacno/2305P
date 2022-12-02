1a9c9ac76da00d326e2cb01ec1f3c4299027adb7
Camila Nepomuceno <camila.cno@gmail.com>
2022-12-02 19:00:03 -0300

    Create storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,10 +1,6 @@
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$gray800',
 })
-
-export function App() {
-  return <Button>Hello World</Button>
-}
