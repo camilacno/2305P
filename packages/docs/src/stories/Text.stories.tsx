ae2acd6c0af65d9fc226521293e656bea2d4fbc3
Camila Nepomuceno <camila.cno@gmail.com>
2022-12-03 15:23:13 -0300

    Box component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import type { StoryObj, Meta } from '@storybook/react' // tipagens do ts
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/text',
+  component: Text,
+
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nesciunt aperiam obcaecati libero similique expedita at ipsa, odit nam itaque totam hic ipsum asperiores deleniti architecto quibusdam ducimus eos nobis.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
