0808e707a61b68a3db4646e9c5a938120d16c82e
Camila Nepomuceno <camila.cno@gmail.com>
2022-12-02 21:10:33 -0300

    Tokens documentation

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import type { StoryObj, Meta } from '@storybook/react' // tipagens do ts
 import { Button, ButtonProps } from '@ignite-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
 
   args: {
